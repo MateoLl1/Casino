f436d9f386ecee0e1dd99164310ebb58dff1c691
Mateo Llerena <mateollerena40@gmail.com>
2023-03-30 13:06:57 -0600

    1C Casino version 1.00

---
+++ src/clase/imagen1.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase;
+
+import java.util.Random;
+import javax.swing.JLabel;
+
+public class imagen1 extends Thread {
+
+    int aleatorio;
+
+    javax.swing.JLabel imagen1;
+
+    public void run() {
+        Random azar = new Random();
+
+        while (true) {
+            aleatorio = azar.nextInt(4);
+            if (aleatorio == 0) {
+                rsscalelabel.RSScaleLabel.setScaleLabel(imagen1, "src/imagenes/fresa.png");
+            }
+            if (aleatorio == 1) {
+                rsscalelabel.RSScaleLabel.setScaleLabel(imagen1, "src/imagenes/moneda.png");
+            }
+            if (aleatorio == 2) {
+                rsscalelabel.RSScaleLabel.setScaleLabel(imagen1, "src/imagenes/esmeralda.png");
+            }
+            if (aleatorio == 3) {
+                rsscalelabel.RSScaleLabel.setScaleLabel(imagen1, "src/imagenes/calavera.png");
+            }
+            try {
+                sleep(1000);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public void setImagen1(JLabel imagen1) {
+        this.imagen1 = imagen1;
+    }
+
+    public int getAleatorio() {
+        return aleatorio;
+    }
+
+    
+
+    
+
+   
+
+}
